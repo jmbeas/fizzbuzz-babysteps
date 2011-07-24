4a57d7b877c3d44e10bf192319de9bfafd87a5c8
jmbeas <jose.m.beas@gmail.com>
2011-07-24 02:51:45 +0200

    Test for 15 (to check FizzBuzz). RED

--- test/test_fizzbuzz.rb
+++ test/test_fizzbuzz.rb
@@ -27,6 +27,10 @@ class TestFizzbuzz < Test::Unit::TestCase
     assert_equal "Buzz", FizzBuzz.say(10)
   end
 
+  should "say FizzBuzz for multiples of 3 and 5" do
+    assert_equal "FizzBuzz", FizzBuzz.say(15)
+  end
+
   should "say the number for the rest" do
     assert_equal "1", FizzBuzz.say(1)
     assert_equal "2", FizzBuzz.say(2)
