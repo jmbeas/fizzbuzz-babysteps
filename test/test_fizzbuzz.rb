dc98d6593a8eb6c0c9b51df237807f1939de01e2
jmbeas <jose.m.beas@gmail.com>
2011-08-14 01:07:43 +0200

    say 1 for 1. GREEN

--- test/test_fizzbuzz.rb
+++ test/test_fizzbuzz.rb
@@ -2,7 +2,13 @@ require "test/unit"
 require "shoulda"
 
 class TestFizzbuzz < Test::Unit::TestCase
+
+  def say(n)
+    "1"
+  end
+
   should "say 1 for 1" do
     assert_equal "1", say(1)
   end
+
 end
