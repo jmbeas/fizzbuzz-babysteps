681384d65ce11a6049a3b4e8ea53667f202d7945
jmbeas <jose.m.beas@gmail.com>
2011-08-14 04:41:28 +0200

    Everyting fits now in the same pattern and we are ready to go for a list of rules

--- test/test_fizzbuzz.rb
+++ test/test_fizzbuzz.rb
@@ -6,11 +6,11 @@ class TestFizzbuzz < Test::Unit::TestCase
   MESSAGE_WHEN_NUMBER_IS_FIZZY = "Fizz"
   MESSAGE_WHEN_NUMBER_IS_BUZZY = "Buzz"
 
-  def message_when_number_is_fizzy
+  def message_when_number_is_fizzy(n)
     MESSAGE_WHEN_NUMBER_IS_FIZZY
   end
 
-  def message_when_number_is_buzzy
+  def message_when_number_is_buzzy(n)
     MESSAGE_WHEN_NUMBER_IS_BUZZY
   end
 
@@ -31,8 +31,8 @@ class TestFizzbuzz < Test::Unit::TestCase
   end
 
   def say(n)
-    return message_when_number_is_fizzy if is_fizzy? n
-    return message_when_number_is_buzzy if is_buzzy? n
+    return message_when_number_is_fizzy(n) if is_fizzy? n
+    return message_when_number_is_buzzy(n) if is_buzzy? n
     return message_when_default_case(n) if is_default_case? n
   end
 
